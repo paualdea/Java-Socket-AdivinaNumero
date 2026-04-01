45cfe4dd50ed5e76e5da1f79ca33d3ed7cf779b7
Pau Aldea Batista <paualdeabatista@protonmail.com>
2026-04-02 00:38:59 +0200

    Mejoras en el código, mejorado de errores técnicos

--- src/main/java/ut3/act1/Servidor.java
+++ src/main/java/ut3/act1/Servidor.java
@@ -40,51 +40,56 @@ public class Servidor implements Runnable {
      */
     @Override
     public void run() {
-        while (!estado.equals("acertado")) {
-            // Implementamos una estructura try-with-resources para optimizar los recursos y manejar excepciones
-            try (
-                    // Creamos el socket servidor (ServerSocket) y el socket que contactara con el cliente (Socket)
-                    ServerSocket servidor = new ServerSocket(puerto);
-                    Socket socket = servidor.accept();
+        // Creamos el ServerSocket una sola vez
+        try (ServerSocket servidor = new ServerSocket(puerto)) {
+            while (!estado.equals("acertado")) {
+                // Implementamos una estructura try-with-resources para optimizar los recursos y manejar excepciones
+                try (
+                        // Creamos socket que contactara con el cliente
+                        Socket socket = servidor.accept();
 
-                    // Creamos el lector y escritor que interactuán con el cliente
-                    BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true)
-            ){
-                // Obtenemos el numero mandado por el cliente y lo transformamos en entero
-                String numeroClienteString = lector.readLine();
+                        // Creamos el lector y escritor que interactuán con el cliente
+                        BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true)
+                ){
+                    // Obtenemos el numero mandado por el cliente y lo transformamos en entero
+                    String numeroClienteString = lector.readLine();
 
-                // Si el lector no esta vacío, empezar a leerlo
-                if (numeroClienteString != null) {
-                    int numeroCliente = Integer.parseInt(numeroClienteString);
+                    // Si el lector no esta vacío, empezar a leerlo
+                    if (numeroClienteString != null) {
+                        int numeroCliente = Integer.parseInt(numeroClienteString);
 
-                    // si el numero del cliente es correcto, acabamos el programa
-                    if (numeroCliente == numero) {
-                        System.err.println("\nSERVIDOR: ¡Numero acertado! Era el " + numero);
+                        // si el numero del cliente es correcto, acabamos el programa
+                        if (numeroCliente == numero) {
+                            System.out.println("\nSERVIDOR: ¡Numero acertado! Era el " + numero);
 
-                        // Establecemos estado en acertado para salir del bucle
-                        estado = "acertado";
-                        escritor.println(estado);
-                        // Si el numero del cliente es mas pequeño que el correcto, mandamos el estado pequeño
-                    } else if (numeroCliente < numero) {
-                        System.err.println("SERVIDOR: El numero correcto es mas grande que " + numeroCliente);
-                        estado = "pequeno";
-                        escritor.println(estado);
-                        // Por otro lado, si el numero del cliente es demasiado grande, mandamos estado grande
-                    } else {
-                        System.err.println("SERVIDOR: El numero correcto es mas pequeño que " + numeroCliente);
-                        estado = "grande";
-                        escritor.println(estado);
+                            // Establecemos estado en acertado para salir del bucle
+                            estado = "acertado";
+                            escritor.println(estado);
+                            // Si el numero del cliente es mas pequeño que el correcto, mandamos el estado pequeño
+                        } else if (numeroCliente < numero) {
+                            System.out.println("SERVIDOR: El numero correcto es mas grande que " + numeroCliente);
+                            estado = "pequeno";
+                            escritor.println(estado);
+                            // Por otro lado, si el numero del cliente es demasiado grande, mandamos estado grande
+                        } else {
+                            System.out.println("SERVIDOR: El numero correcto es mas pequeño que " + numeroCliente);
+                            estado = "grande";
+                            escritor.println(estado);
+                        }
+                    }
+                    // En caso de que no podamos leer nada del cliente, devolvemos nulo para que no ocurra nada
+                    else {
+                        escritor.println("nulo");
                     }
                 }
-                // En caso de que no podamos leer nada del cliente, devolvemos nulo para que no ocurra nada
-                else {
-                    escritor.println("nulo");
+                catch (IOException e) {
+                    System.err.println("Problema en la comunicación cliente-servidor: " + e.getLocalizedMessage());
                 }
             }
-            catch (IOException e) {
-                System.err.println("Problema en la comunicación cliente-servidor: " + e.getLocalizedMessage());
-            }
+        }
+        catch (IOException e) {
+            System.err.println("Error al abrir el servidor: " + e.getMessage());
         }
     }
 }
